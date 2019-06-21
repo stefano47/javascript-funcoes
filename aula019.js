f6d3887ce2395da6407cf9851ffcbd6c591a4375
Stefano <stefanopaulo47@gmail.com>
2019-06-21 14:50:59 -0300

    funcoes factory

---
+++ aula019.js
@@ -0,0 +1,11 @@
+// função factory #01
+// É uma Função que retorna um Objeto
+
+function criarPessoa(){
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa())
