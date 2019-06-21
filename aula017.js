7b6476acff11737ecb22daa511a5da94862edfce
Stefano <stefanopaulo47@gmail.com>
2019-06-21 14:39:47 -0300

    contexto lexico

---
+++ aula017.js
@@ -0,0 +1,16 @@
+// contexto léxico
+
+const valor = 'Global'
+
+function minhaFuncao(){
+    console.log(valor)
+}
+
+minhaFuncao()
+
+function exec() {
+    const valor = 'Local'
+    minhaFuncao()
+}
+
+exec()
